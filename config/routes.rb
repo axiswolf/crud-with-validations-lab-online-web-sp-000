026f7f906f3fbc1cae35994263296e867eb98ec4
Karen V <karenvo@usc.edu>
2019-12-03 08:58:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :songs, only: [:index, :show, :create, :edit, :update]
-  delete '/song/:id', to: 'song#destroy', as: 'song'
+  resources :songs, only: [:index, :show, :create, :edit, :update, :delete]
+#  delete '/songs/:id', to: 'song#destroy', as: 'song'
 end
