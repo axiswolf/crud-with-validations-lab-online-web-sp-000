bedab3dc86f2fb7675577db8e7600756e1e38840
Karen V <karenvo@usc.edu>
2019-12-03 08:28:46 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,4 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :release_year, presence: true, uniqueness: true
+  validates :release_year, presence: true, uniqueness: true, in: 1900..2019
 end
