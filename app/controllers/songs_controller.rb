2ea40e0f13cb587909b833bf1b536089112552a1
Karen V <karenvo@usc.edu>
2019-12-03 08:42:20 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,13 +14,13 @@ class SongsController < ApplicationController
 
   private
 
-  
-    def set_song 
+
+    def set_song
       @song = Song.find(params[:id])
-    end 
-  
-    def song_params 
+    end
+
+    def song_params
       params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
     end
-    
+
 end
