d01785444c4a114dc396e660ce6a60fbdb9b6c1b
Karen V <karenvo@usc.edu>
2019-12-03 08:53:24 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,6 +22,17 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+  def update 
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+  
   private
 
     def set_song
